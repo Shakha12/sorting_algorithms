d33358ed383982337bdc13ea10fcdb5e4e2a4310
frankMnjoroge <frankmugo@gmail.com>
2023-12-19 15:22:34 -0800

    updated

--- sort.h
+++ sort.h
@@ -2,6 +2,21 @@
 #define __SORT__
 #include <stdlib.h>
 
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -16,17 +31,12 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/*======================================================================*/
-/*=================         Printing Algoritms        ==================*/
-/*======================================================================*/
-
+/*Printing Algoritms*/
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 
-/*======================================================================*/
-/*=================          sorting Algoritms        ==================*/
-/*======================================================================*/
+/*sorting Algoritms*/
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
