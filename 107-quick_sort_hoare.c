d350c6d6c6df33c66fbc17517ec3f0fc834a3270
root <gicherushadrack@gmail.com>
2023-12-19 16:04:45 -0800

    variables

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,6 +1,6 @@
 #include "sort.h"
 
-void swap_ints(int *a, int *b);
+void swap_ints(int *x, int *y);
 int hoare_part(int *array, size_t size, int left, int right);
 void hoare_sort(int *array, size_t size, int left, int right);
 void quick_sort_hoare(int *array, size_t size);
